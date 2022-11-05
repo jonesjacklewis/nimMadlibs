60a00d6f8852f70277771e17919dcc3016dd4753
jonesjacklewis <jones.jack.lewis@gmail.com>
2022-11-05 12:26:23 +0000

    String replacement through for-loop

--- main.nim
+++ main.nim
@@ -4,4 +4,16 @@ let fileContents = readFile("story.txt")
 
 let words = fileContents.split(" ")
 
-echo words
+var story = words
+
+const partsOfSpeech = ["ADJECTIVE", "NOUN", "ADVERB", "VERB"]
+
+for i in countup(0, words.len - 1):
+    var word = words[i]
+
+    if word in partsOfSpeech:
+        echo "Enter a ", word, ": "
+        word = readLine(stdin)
+    story[i] = word
+
+echo story
